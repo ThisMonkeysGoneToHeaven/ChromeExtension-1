e1bce8d64b49ec972f7fc547a680b8df07f910d5
Anant Agarwal <anantagarwal@Anants-MacBook-Air.local>
2023-07-04 02:48:37 +0530

    used chrome extension's messaging api to send data b/w background.js and content-script.js

--- background.js
+++ background.js
@@ -3,9 +3,10 @@ console.log('background.js running');
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 async function buttonClicked(tab){
+    console.log('extension button clicked!');
     const mssg = {
         txt: "hello"
     };
     const response = await chrome.tabs.sendMessage(tab.id, mssg);
-    console.log('response in background script - ' + response);
+    // console.log('response in background script - ' + response);
 }
--- content.js
+++ content.js
@@ -1,6 +1,17 @@
 console.log('Member of the Panda Community!');
 
-let paragrahps = document.getElementsByTagName('p');
-for (elem of paragrahps){
-    elem.style['background-color'] = '#FF00FF';
+function changeParasToPurple(){
+    let paragrahps = document.getElementsByTagName('p');
+    for (elem of paragrahps){
+        elem.style['background-color'] = '#FF00FF';
+        }
 }
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        console.log('mssg received at content-script : ' + request.txt);
+        if(request.txt === 'hello'){
+            changeParasToPurple();
+        }
+    }
+);
